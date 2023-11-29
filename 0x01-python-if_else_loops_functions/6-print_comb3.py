e14254565f2509f1ffb0ac63a7aafad96e710209
mg-jnr <michaelgozo4@gmail.com>
2023-11-29 03:28:20 -0800

    2 num combo (01 and 10 are considered the same)

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range (10):
+    for j in range(10):
+        if i != j and i < j and i < 8:
+            print("{}{}, ".format(i,j),end="")
+        elif i != j and i < j and j == 9:
+            print("{}{}".format(i,j))
