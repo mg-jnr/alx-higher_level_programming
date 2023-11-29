9840d866fc161ab05382a9364572b6675edd037c
mg-jnr <michaelgozo4@gmail.com>
2023-11-29 05:47:36 -0800

    find last number

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number < 0:
+        lastDigit = (number * -1) % 10
+        print(f"{lastDigit}", end='')
+    elif number >= 0:
+        lastDigit = number % 10
+        print(f"{lastDigit}", end='')
+    return(lastDigit)
